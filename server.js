0a04b5ac5317761c88c528ec1e0cac3e001dcbe8
Ryan Lewis <rylewis@expedia.com>
2015-07-09 12:55:31 -0700

    simple express app

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
-var request = require('request-json');
+var https = require('https'),
+	url = require('url'),
+	express = require('express');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-var client = request.createClient('https://jira.sea.corp.expecn.com');
+function getData(callback) {
+	var options = {
+		auth: 's-nero:$A#dxPZ@3',
+		hostname: 'jira',
+		pathname: '/jira/rest/api/2/issue/1051293',
+		protocol: 'https:'
+	};
 
-client.setBasicAuth('s-nero', '$A#dxPZ@3');
+	https.get(url.format(options), function(res) {
+		var data = '';
+		res.on('data', function(d) {
+		    data += d;
+		});
+		res.on('end', function(d) {
+			if(d) {
+				data += d;
+			}
+			callback(data);
+		});
+	}).on('error', function(e) {
+		console.log('Error: ' + e);
+	});
+}
 
-client.get('jira/rest/api/2/issue/1051293', function (err, res, body) {
-    return console.log(body.rows[0].title);
+var app = express();
+
+app.get('/', function(req, res) {
+	getData(function(data) {
+		res.json(JSON.parse(data));
+	});
 });
+
+app.listen(3000);
+
+
+
+
+
+
+
+
