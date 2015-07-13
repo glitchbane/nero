f69987eeba1aa1e7e0897e00db47a925a88cce39
Ryan Lewis <rylewis@expedia.com>
2015-07-13 12:58:20 -0700

    task lists by sprint/team

--- server.js
+++ server.js
@@ -1,42 +1,31 @@
-var https = require('https'),
-	url = require('url'),
+var JiraData = require('./lib/JiraData'),
 	express = require('express');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-function getData(callback) {
-	var options = {
-		auth: 's-nero:$A#dxPZ@3',
-		hostname: 'jira',
-		pathname: '/jira/rest/api/2/issue/1051293',
-		protocol: 'https:'
-	};
-
-	https.get(url.format(options), function(res) {
-		var data = '';
-		res.on('data', function(d) {
-		    data += d;
-		});
-		res.on('end', function(d) {
-			if(d) {
-				data += d;
-			}
-			callback(data);
-		});
-	}).on('error', function(e) {
-		console.log('Error: ' + e);
-	});
-}
-
 var app = express();
 
 app.get('/', function(req, res) {
-	getData(function(data) {
-		res.json(JSON.parse(data));
-	});
+	JiraData.getTasks('Alliance', 
+		'Sprint 77', 
+		function(data) {
+			console.log(data);
+			res.json(JSON.parse(data));
+		});
 });
 
-app.listen(3000);
+app.get('/tasks/:team/:sprint', function(req, res) {
+	JiraData.getTasks(req.params.team, 
+		req.params.sprint, 
+		function(data) {
+			console.log(data);
+			res.json(JSON.parse(data));
+		});
+});
+
+app.listen(3000, function() {
+	console.log('listening');
+});
 
 
 
