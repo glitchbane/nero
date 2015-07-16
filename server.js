f961b6447d9a54d8353716a2b968ac803d6aa0df
Sheila Leverson <sleverson@expedia.com>
2015-07-16 08:37:57 -0700

    do not merge until it works right.  This commit adds a method to get all the teams in the given sprint

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var app = express();
 app.use(logger('dev'));
 
-var portNumber = 4000;
+var portNumber = 3000;
 
 app.get('/', function(req, res) {
     JiraData.getTasks('Alliance',
@@ -25,8 +25,22 @@ app.get('/tasks/:team/:sprint', function(req, res) {
         });
 });
 
+app.get('/tasks/:sprint', function(req, res) {
+    JiraData.getSprintTasks(req.params.sprint,
+        function(data) {
+            res.json(JSON.parse(data));
+        });
+});
+
 app.get('/teams', function(req, res) {
     JiraData.getTeams(
+        function(data) {
+            res.json(data);
+        });
+});
+
+app.get('/teams/:sprint', function(req, res) {
+    JiraData.getSprintTeams(req.params.sprint,
         function(data) {
             res.json(data);
         });
