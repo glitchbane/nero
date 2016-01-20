182ee27afcce25a80828dff83652d5f7b0fd3a3b
unknown <sleverson@BELR9010FWN.SEA.CORP.EXPECN.com>
2016-01-20 14:35:12 -0800

    added ability to get teams from mongodb

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,19 @@ $('#queryBtn').click(function() {
             unload: chart.columns});
     });
 })
+
+
+// populate team List
+// user selects team
+// sprint list is populated with sprints that are valid for that team
+// $('#teamsDropdown').on('change', function() {
+//     var selectedTeam = $('#teamsDropdown').find('select').val();
+//     $.getJSON('/team/' + selectedTeam + '/sprints', function(sprintsData) {
+//         var ddlMarkup = '<select>';
+//         $.each(sprintsData, function(index, sprint) {
+//             ddlMarkup += '<option>' + sprint.name + '</option>';
+//         })
+//        ddlMarkup += '</select>';
+//        $('#sprintsDropdown').html(ddlMarkup);
+//     })
+// });
