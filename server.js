f272fff33c198c373934381205d9c2d0da2d0167
unknown <sleverson@BELR9010FWN.SEA.CORP.EXPECN.com>
2016-01-21 14:36:42 -0800

    added sample data files and agile api methods

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var logger = require('morgan'),
     express = require('express'),
     routes = require('./routes/routes'),
-    cons = require('consolidate');
+    cons = require('consolidate'),
+    mongoClient = require('mongodb').MongoClient;
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -23,6 +24,7 @@ app.get('/tasks/:sprint', routes.tasksSprint);
 app.get('/teams', routes.teams);
 app.get('/teams/:sprint', routes.teamsSprint);
 app.get('/sprints', routes.sprints);
+app.get('/teams/:teamId/sprints', routes.sprintsForTeam);
 app.get('/chartData/:team/:sprint', routes.chartData);
 app.get('/chartData', routes.chartData)
 
